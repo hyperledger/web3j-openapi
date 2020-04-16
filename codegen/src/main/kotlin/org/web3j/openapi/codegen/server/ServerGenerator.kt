25ca885a9e8db3954a8832855b88f8694548accc
Chami Rachid <chamirachid1@gmail.com>
2020-04-16 09:32:11 -0700

    Added partial server module generator

--- codegen/src/main/kotlin/org/web3j/openapi/codegen/server/ServerGenerator.kt
+++ codegen/src/main/kotlin/org/web3j/openapi/codegen/server/ServerGenerator.kt
@@ -1,16 +1,4 @@
-/*
- * Copyright 2020 Web3 Labs Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
- */
-package org.web3j.openapi.codegen.client
+package org.web3j.openapi.codegen.server
 
 import org.web3j.openapi.codegen.DefaultGenerator
 import org.web3j.openapi.codegen.config.GeneratorConfiguration
@@ -18,17 +6,18 @@ import org.web3j.openapi.codegen.utils.CopyUtils
 import org.web3j.openapi.codegen.utils.TemplateUtils
 import java.io.File
 
-class ClientGenerator(
+class ServerGenerator (
     override val configuration: GeneratorConfiguration
 ) : DefaultGenerator(
     configuration
 ) {
     override val packageDir = configuration.packageName.split(".").joinToString("/")
-    override val folderPath = CopyUtils.createTree("client", packageDir, configuration.outputDir)
+    override val folderPath = CopyUtils.createTree("server", packageDir, configuration.outputDir)
 
     override fun generate() {
         copyGradleFile(folderPath)
         val context = setContext()
+        copyResources()
         copySources(context)
     }
 
@@ -37,16 +26,34 @@ class ClientGenerator(
     }
 
     private fun copyGradleFile(folderPath: String) {
-        logger.debug("Copying client/build.gradle")
+        logger.debug("Copying server/build.gradle")
         CopyUtils.copyResource(
-            "client/build.gradle",
-            File(folderPath.substringBefore("client")))
+            "server/build.gradle",
+            File(folderPath.substringBefore("server"))
+        )
+    }
+
+    private fun copyResources() {
+        File("${folderPath.substringBefore("main")}${File.separator}main${File.separator}resources")
+            .apply {
+                mkdirs()
+            }
+        logger.debug("Copying server/resources")
+        CopyUtils.copyResource(
+            "server/src/main/resources/logback.xml",
+            File(folderPath.substringBefore("server"))
+        )
+        CopyUtils.copyResource(
+            "server/src/main/resources/logging.properties",
+            File(folderPath.substringBefore("server"))
+        )
     }
 
     private fun copySources(context: HashMap<String, Any>) {
-        File("codegen/src/main/resources/client/src/")
+        File("codegen/src/main/resources/server/src/")
             .listFiles()
-            ?.forEach { it ->
+            .filter { !it.isDirectory }
+            .forEach {
                 logger.debug("Generating from ${it.canonicalPath}")
                 TemplateUtils.generateFromTemplate(
                     context = context,
