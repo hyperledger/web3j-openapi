9c9395fa68c8d637b4f00792ae76ce3470c09aa9
Chami Rachid <chamirachid1@gmail.com>
2020-04-17 02:37:32 -0700

    Added partial contracts.server generator

--- codegen/src/main/kotlin/org/web3j/openapi/codegen/server/ServerGenerator.kt
+++ codegen/src/main/kotlin/org/web3j/openapi/codegen/server/ServerGenerator.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2020 Web3 Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.web3j.openapi.codegen.server
 
 import org.web3j.openapi.codegen.DefaultGenerator
@@ -6,7 +18,7 @@ import org.web3j.openapi.codegen.utils.CopyUtils
 import org.web3j.openapi.codegen.utils.TemplateUtils
 import java.io.File
 
-class ServerGenerator (
+class ServerGenerator(
     override val configuration: GeneratorConfiguration
 ) : DefaultGenerator(
     configuration
