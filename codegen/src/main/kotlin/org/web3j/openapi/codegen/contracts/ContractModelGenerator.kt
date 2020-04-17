c7d9f18b932d15264dd21e472a57ca2d6c1ab8d1
Chami Rachid <chamirachid1@gmail.com>
2020-04-17 03:28:38 -0700

    Added partial contracts.contact.api.model generator

--- codegen/src/main/kotlin/org/web3j/openapi/codegen/contracts/ContractModelGenerator.kt
+++ codegen/src/main/kotlin/org/web3j/openapi/codegen/contracts/ContractModelGenerator.kt
@@ -12,4 +12,46 @@
  */
 package org.web3j.openapi.codegen.contracts
 
-class ContractModelGenerator
+import org.slf4j.Logger
+import org.web3j.openapi.codegen.config.ContractDetails
+import org.web3j.openapi.codegen.utils.TemplateUtils
+import java.io.File
+
+class ContractModelGenerator(
+    val packageName: String,
+    val folderPath: String,
+    val logger: Logger,
+    val contractDetails: ContractDetails
+) {
+    fun generate() {
+        File("$folderPath${File.separator}api${File.separator}model")
+            .apply {
+                mkdirs()
+            }
+        val context = setContext()
+        copySources(context)
+    }
+
+    private fun setContext(): HashMap<String, Any> {
+        return hashMapOf(
+            "packageName" to packageName,
+            "lowerCaseContractName" to contractDetails.lowerCaseContractName(),
+            "capitalizedContractName" to contractDetails.capitalizedContractName()
+        )
+    }
+
+    private fun copySources(context: HashMap<String, Any>) {
+        File("codegen/src/main/resources/contracts/src/model")
+            .listFiles()
+            ?.forEach {
+                logger.debug("Generating from ${it.canonicalPath}")
+                TemplateUtils.generateFromTemplate(
+                    context = context,
+                    outputDir = "$folderPath${File.separator}api${File.separator}model",
+                    template = TemplateUtils.mustacheTemplate(it.path.substringAfter("resources/")),
+                    name = "${contractDetails.capitalizedContractName()}${it.name.removeSuffix(".mustache")
+                        .removePrefix("Contract")}.kt"
+                )
+            }
+    }
+}
