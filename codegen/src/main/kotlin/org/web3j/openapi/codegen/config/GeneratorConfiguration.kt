fbfd8cf5817ce9e66779c9f251f973b551316d66
Chami Rachid <chamirachid1@gmail.com>
2020-04-27 04:20:57 -0700

    Removed unnecessary GeneratorConfiguration field

--- codegen/src/main/kotlin/org/web3j/openapi/codegen/config/GeneratorConfiguration.kt
+++ codegen/src/main/kotlin/org/web3j/openapi/codegen/config/GeneratorConfiguration.kt
@@ -13,7 +13,6 @@
 package org.web3j.openapi.codegen.config
 
 data class GeneratorConfiguration(
-    val projectName: String,
     val packageName: String,
     val outputDir: String,
     val contracts: List<ContractConfiguration>
