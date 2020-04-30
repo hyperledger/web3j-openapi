481c53d3a49cb5712fbff8f7232d0262113b6f38
Chami Rachid <chamirachid1@gmail.com>
2020-04-29 17:20:38 -0700

    todo

--- server/src/main/kotlin/org/web3j/openapi/server/Main.kt
+++ server/src/main/kotlin/org/web3j/openapi/server/Main.kt
@@ -57,7 +57,7 @@ fun main() {
         registerClasses(OpenApiResource::class.java)
     }
 
-    ServiceLoader.load(OpenApiResourceProvider::class.java).forEach {
+    ServiceLoader.load(OpenApiResourceProvider::class.java).forEach { //TODO: Add registration logs
         resourceConfig.register(it.get())
     }
 
