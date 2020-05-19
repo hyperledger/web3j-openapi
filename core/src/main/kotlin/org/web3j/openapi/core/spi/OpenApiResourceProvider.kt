a71c3c58a5d137ef0cfe26807b0a2b3ca965090a
Chami Rachid <chamirachid1@gmail.com>
2020-05-19 01:45:59 -0700

    cleaning

--- core/src/main/kotlin/org/web3j/openapi/core/spi/OpenApiResourceProvider.kt
+++
@@ -1,6 +0,0 @@
-package org.web3j.openapi.core.spi
-
-import org.web3j.openapi.core.Web3jOpenApi
-import java.util.function.Supplier
-
-interface OpenApiResourceProvider: Supplier<Class<out Web3jOpenApi>>
