5252ef544518e7c9bb0fbfb87c85ac02776569f8
Chami Rachid <chamirachid1@gmail.com>
2020-05-19 01:45:59 -0700

    cleaning

--- helloworld/src/main/kotlin/org/web3j/openapi/helloworld/spi/HelloWorldResourceProvider.kt
+++
@@ -1,8 +0,0 @@
-package org.web3j.openapi.helloworld.spi
-
-import org.web3j.openapi.core.spi.OpenApiResourceProvider
-import org.web3j.openapi.helloworld.server.HelloWorldApiImpl
-
-class HelloWorldResourceProvider : OpenApiResourceProvider {
-    override fun get() = HelloWorldApiImpl::class.java
-}
