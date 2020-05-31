54e68f9ef169ba689a996416c2568527f4e97763
Xavier Arias <xavier@web3labs.com>
2020-05-31 19:03:44 +0100

    Remove main method, will be created by the CLI

--- server/src/main/kotlin/org/web3j/openapi/server/OpenApiServer.kt
+++ server/src/main/kotlin/org/web3j/openapi/server/OpenApiServer.kt
@@ -22,7 +22,6 @@ import org.eclipse.jetty.util.Loader.getResource
 import org.eclipse.jetty.util.resource.Resource
 import org.glassfish.jersey.servlet.ServletContainer
 import org.web3j.openapi.core.spi.OpenApiResourceProvider
-import org.web3j.openapi.server.cli.OpenApiServerCommand
 import org.web3j.openapi.server.config.OpenApiResourceConfig
 import org.web3j.openapi.server.config.OpenApiServerConfig
 import java.net.URI
@@ -65,8 +64,4 @@ class OpenApiServer(private val serverConfig: OpenApiServerConfig) : Server() {
             }
         }
     }
-}
-
-fun main(args: Array<String>) {
-    OpenApiServerCommand().parse(*args)
 }
