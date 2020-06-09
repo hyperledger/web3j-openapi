a231c5afe741c26b7eb177db2d9bdca77590929c
Chami Rachid <chamirachid1@gmail.com>
2020-06-09 10:17:33 -0700

    spotless

--- codegen/src/test/kotlin/org/web3j/openapi/codegen/generators/core/ModelsTests.kt
+++ codegen/src/test/kotlin/org/web3j/openapi/codegen/generators/core/ModelsTests.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2020 Web3 Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.web3j.openapi.codegen.generators.core
 
 import assertk.assertThat
@@ -7,7 +19,9 @@ import org.junit.jupiter.api.io.TempDir
 import org.web3j.openapi.codegen.coregen.subgenerators.CoreDeployModelGenerator
 import org.web3j.openapi.codegen.coregen.subgenerators.CoreEventsModelGenerator
 import org.web3j.openapi.codegen.coregen.subgenerators.CoreFunctionsModelGenerator
+import org.web3j.openapi.codegen.coregen.subgenerators.CoreStructsModelGenerator
 import org.web3j.openapi.codegen.utils.GeneratorUtils
+import org.web3j.openapi.codegen.utils.extractStructs
 import java.io.File
 import java.io.FileNotFoundException
 import java.nio.file.Path
@@ -78,7 +92,7 @@ class ModelsTests {
             assertThat {
                 contractConfiguration
                     .contractDetails
-                    .abiDefinitions.firstOrNull { it.type == "event"}
+                    .abiDefinitions.firstOrNull { it.type == "event" }
                     ?.let {
                         CoreEventsModelGenerator(
                             "com.test",
--- codegen/src/test/kotlin/org/web3j/openapi/codegen/generators/server/ServergenTests.kt
+++ codegen/src/test/kotlin/org/web3j/openapi/codegen/generators/server/ServergenTests.kt
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2020 Web3 Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.web3j.openapi.codegen.generators.server
 
 import assertk.assertThat
@@ -6,7 +18,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import org.web3j.openapi.codegen.config.GeneratorConfiguration
 import org.web3j.openapi.codegen.servergen.ServerGenerator
-import org.web3j.openapi.codegen.servergen.subgenerators.ResourcesImplGenerator
 import org.web3j.openapi.codegen.utils.GeneratorUtils
 import java.io.File
 import java.io.FileNotFoundException
@@ -42,7 +53,7 @@ class ServergenTests {
                     tempFolder.canonicalPath,
                     contractsConfiguration,
                     20,
-                     "0.1.0-SNAPSHOT"
+                    "0.1.0-SNAPSHOT"
                 )
             ).generate()
         }.isSuccess()
