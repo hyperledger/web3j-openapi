0705953973978cad6b2de67e0d2e543f2b68ee32
Xavier Arias <xavier@web3labs.com>
2020-06-09 14:21:35 +0100

    Spotless apply

--- codegen/src/main/kotlin/org/web3j/openapi/codegen/servergen/subgenerators/StructExtensionsGenerator.kt
+++ codegen/src/main/kotlin/org/web3j/openapi/codegen/servergen/subgenerators/StructExtensionsGenerator.kt
@@ -1,6 +1,20 @@
+/*
+ * Copyright 2020 Web3 Labs Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.web3j.openapi.codegen.servergen.subgenerators
 
-import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
 import org.web3j.openapi.codegen.utils.CopyUtils
 import org.web3j.openapi.codegen.utils.extractStructs
 import org.web3j.openapi.codegen.utils.structName
@@ -11,11 +25,12 @@ class StructExtensionsGenerator(
     val packageName: String,
     private val contractName: String,
     private val resourcesDefinition: List<AbiDefinition>,
-    private val folderPath: String) {
+    private val folderPath: String
+) {
 
     fun generate() {
         val structs = extractStructs(resourcesDefinition)
-        if(!structs.isNullOrEmpty())generateExtensions(structs).writeTo(File(folderPath))
+        if (!structs.isNullOrEmpty())generateExtensions(structs).writeTo(File(folderPath))
         File(folderPath)
             .walkTopDown()
             .filter { file -> file.name.endsWith(".kt") }
@@ -58,8 +73,8 @@ class StructExtensionsGenerator(
     }
 
     private fun extensionDefinitionParameters(structDefinition: AbiDefinition.NamedType): String {
-        return structDefinition.components.joinToString (",") {structField ->
-            if(structField.components.isNullOrEmpty())
+        return structDefinition.components.joinToString(",") { structField ->
+            if (structField.components.isNullOrEmpty())
                 structField.name
             else
                 "${structField.name}.toModel()"
