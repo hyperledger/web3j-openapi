ce414ad472796bc8e58d2a9000701d2b9b06eef5
Chami Rachid <chamirachid1@gmail.com>
2020-06-22 08:21:27 -0700

    lowercase package name

--- codegen/src/main/kotlin/org/web3j/openapi/codegen/config/GeneratorConfiguration.kt
+++ codegen/src/main/kotlin/org/web3j/openapi/codegen/config/GeneratorConfiguration.kt
@@ -16,7 +16,7 @@ import org.web3j.abi.datatypes.Address
 
 data class GeneratorConfiguration(
     val projectName: String,
-    val packageName: String,
+    var packageName: String,
     val outputDir: String,
     val contracts: List<ContractConfiguration>,
     val addressLength: Int = Address.DEFAULT_LENGTH / java.lang.Byte.SIZE,
@@ -24,4 +24,8 @@ data class GeneratorConfiguration(
     val version: String
 ) {
     val rootProjectName = projectName.toLowerCase().replace(' ', '-')
+
+    init {
+        packageName = packageName.toLowerCase()
+    }
 }
