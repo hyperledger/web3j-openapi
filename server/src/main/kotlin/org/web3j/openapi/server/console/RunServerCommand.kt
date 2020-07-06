0b29dd211c00674a38b84c2ca21e1bfa90dca836
Ivaylo Kirilov <iikirilov@gmail.com>
2020-07-06 11:58:17 +0100

    refactor/move-cli-to--epirus

--- server/src/main/kotlin/org/web3j/openapi/server/console/RunServerCommand.kt
+++ server/src/main/kotlin/org/web3j/openapi/server/console/RunServerCommand.kt
@@ -10,13 +10,13 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.web3j.openapi.console
+package org.web3j.openapi.server.console
 
-import org.web3j.openapi.console.options.ConfigFileOptions
-import org.web3j.openapi.console.options.CredentialsOptions
-import org.web3j.openapi.console.options.NetworkOptions
-import org.web3j.openapi.console.options.ProjectOptions
-import org.web3j.openapi.console.options.ServerOptions
+import org.web3j.openapi.server.console.options.ConfigFileOptions
+import org.web3j.openapi.server.console.options.CredentialsOptions
+import org.web3j.openapi.server.console.options.NetworkOptions
+import org.web3j.openapi.server.console.options.ProjectOptions
+import org.web3j.openapi.server.console.options.ServerOptions
 import org.web3j.openapi.server.OpenApiServer
 import org.web3j.openapi.server.config.OpenApiServerConfig
 import picocli.CommandLine
