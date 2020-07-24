2250dc8ec3f758cff6d88c8bfe67161cee0560bc
Chami Rachid <chamirachid1@gmail.com>
2020-07-24 08:21:26 -0700

    spotless

--- server/src/main/kotlin/org/web3j/openapi/server/ExceptionMappings.kt
+++ server/src/main/kotlin/org/web3j/openapi/server/ExceptionMappings.kt
@@ -19,7 +19,6 @@ import org.web3j.openapi.core.ErrorResponse
 import org.web3j.protocol.exceptions.TransactionException
 import org.web3j.tx.exceptions.ContractCallException
 import javax.servlet.http.HttpServletRequest
-import javax.ws.rs.NotFoundException
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.HttpHeaders
 import javax.ws.rs.core.Response
